f195c490f00b2b32121fffbc0c997d5db736fe3a
Jose Pereira <josep@dei.uc.pt>
2018-09-25 11:17:38 +0100

    Changing the analyzer to be in a loop

--- src/main/java/eubr/atmosphere/tma/analyze/Main.java
+++ src/main/java/eubr/atmosphere/tma/analyze/Main.java
@@ -20,8 +20,15 @@ public class Main {
     public static void main(String[] args) {
         DataManager dataManager = new DataManager();
 
-        calculateScoreNonNormalized(dataManager);
-        calculateScoreNormalized(dataManager);
+        for (int i = 0; i < 60; i++) {
+            calculateScoreNonNormalized(dataManager);
+            calculateScoreNormalized(dataManager);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
