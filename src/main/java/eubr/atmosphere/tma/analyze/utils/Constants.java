032b0b72c9bb6ba4188cc320eaf957c25ee98d70
Jose Pereira <josep@dei.uc.pt>
2018-11-06 09:11:03 +0000

    Changing the pod to be monitored

--- src/main/java/eubr/atmosphere/tma/analyze/utils/Constants.java
+++ src/main/java/eubr/atmosphere/tma/analyze/utils/Constants.java
@@ -9,7 +9,8 @@ public final class Constants {
     public static final int cpuDescriptionId = 27;
     public static final int memoryDescriptionId = 28;
 
-    public static final int podId = 8; //kafka
+    //public static final int podId = 8; // kafka
+    public static final int podId = 10; // wildfly-0
     public static final int nodeId = 11; // worker node
 
     // Metrics related to the node
